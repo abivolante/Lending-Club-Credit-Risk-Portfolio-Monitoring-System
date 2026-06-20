cd82c075e992497056e37582b82c96c4d16dc8c2
Abigail Volante <abigailvolante1@gmail.com>
2026-06-20 11:56:00 +0100

    Create phase_3_loss_distribution.sql

---
+++ sql/phase_3_loss_distribution.sql
@@ -0,0 +1,36 @@
+-- =====================================================
+-- PHASE 3: LOSS DISTRIBUTION
+-- =====================================================
+--
+-- Objective:
+-- To quantify actual financial losses and identify segments contributing most to portfolio loss
+--
+-- =====================================================
+
+--Total charged-off exposure
+SELECT 
+      SUM(loan_amnt) AS total_loss
+ FROM `lending-club-499707.lending_club.loan` 
+ WHERE loan_status='Charged Off'
+
+
+
+--Loss distribution by grade
+SELECT
+    grade,
+    SUM(loan_amnt) AS total_charged_off_exposure
+FROM `lending-club-499707.lending_club.loan`
+WHERE loan_status = 'Charged Off'
+GROUP BY grade
+ORDER BY total_charged_off_exposure DESC;
+
+
+
+--Loss distribution by purpose
+SELECT
+    purpose,
+    SUM(loan_amnt) AS total_charged_off_exposure
+FROM `lending-club-499707.lending_club.loan`
+WHERE loan_status = 'Charged Off'
+GROUP BY purpose
+ORDER BY total_charged_off_exposure DESC;
